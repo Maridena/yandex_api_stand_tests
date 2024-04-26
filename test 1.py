fd3d70fcfba4ffb6fc5ecc3a15cf1c7188b7e86c
marin <marina.zaborowska@gmail.com>
2024-04-26 15:37:32 +0200

    My first commit

---
+++ test 1.py
@@ -0,0 +1,13 @@
+# Тест 1. Успешное создание пользователя
+# Параметр fisrtName состоит из 2 символов
+
+def test_create_user_2_letter_in_first_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса с именем "Аа"
+    user_body = get_user_body("Аа")
+    # В переменную user_response сохраняется результат запроса на создание пользователя
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken и оно не пустое
+    assert user_response.json()["authToken"] != ""
---
+++ test 2.py
@@ -0,0 +1,4 @@
+# Тест 2. Успешное создание пользователя
+# Параметр firstName состоит из 15 символов
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Ааааааааааааааа")
